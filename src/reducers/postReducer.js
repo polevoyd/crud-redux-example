80e0450542570bd06bb63cb600961f17ead4e188
Dmitry <manxxeta@gmail.com>
2018-10-25 14:19:38 -0700

    initial commit

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -3,7 +3,10 @@ const postReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_POST':
             return state.concat([action.data]);
-    
+
+        case 'DELETE_POST':
+            return state.filter((post) => post.id !== action.id);
+            
         default:
             return state;
     }
