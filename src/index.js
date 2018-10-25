8d27218a991f85b0c52980502b1744af3f5058cf
Dmitry <manxxeta@gmail.com>
2018-10-25 13:39:30 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// importing redux...
+// importing redux function to create state 
 import { createStore } from 'redux';
+// importing reducer we made (функция которая возвращает новый стейт)
+import postReducer from './reducers/postReducer';
 
-const store = createStore();
+const store = createStore(postReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
