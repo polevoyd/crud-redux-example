96e6d0b4cd20be4796e0cb644934baa491a91615
Dmitry <manxxeta@gmail.com>
2018-10-25 13:52:57 -0700

    initial commit

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class PostForm extends React.Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const title = this.getTitle.value;
+        const message = this.getMessage.value;
+        const data = {
+            id: new Date(),
+            title,
+            message
+        }
+        console.log(data)
+    }
+
     render() {
         return (
             <div>
                 <h1> Create Post </h1>
-                <form>
-                    <input required type="text" placeholder="Enter Post Title" />
-                    <textarea required rows="5" cols="28" placeholder="Enter Post" />
+                <form onSubmit={this.handleSubmit}>
+                    <input required type="text" ref={(input) => this.getTitle = input} placeholder="Enter Post Title" />
+                    <textarea required rows="5" ref={(input)=> this.getMessage = input} cols="28" placeholder="Enter Post" />
                     <button>Post</button>
                 </form>
             </div>
@@ -15,4 +29,9 @@ class PostForm extends React.Component {
     }
 }
 
-export default PostForm;
+// connect() returns a function which takes in your current component 
+// as an argument and returns a new component with dispatch method as it’s prop. 
+
+export default connect()(PostForm);
+
+// export default PostForm;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import App from './App';
 
 // importing redux function to create state 
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 // importing reducer we made (функция которая возвращает новый стейт)
 import postReducer from './reducers/postReducer';
 
 const store = createStore(postReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Passing that state to main APP component - 
+// to make state available for all childs
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+    ,document.getElementById('root'));
